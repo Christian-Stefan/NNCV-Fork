3732c947da9e0f980232879a58d05cc2c25aa401
Cristian <c.gheorghiu1@student.tue.nl>
2026-03-27 23:37:16 +0100

    <Minor><Weights Update>

--- Final assignment/train.py
+++ Final assignment/train.py
@@ -118,7 +118,8 @@ def main(args):
     ToImage(),
     Resize((256, 512)),
     ToDtype(torch.float32, scale=True),
-    Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), Default normalization
     ])
 
     #TODO Find out what the target_transform do?
